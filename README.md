0f9301a0929f2ee3d86ec19845974c8737402179
Unitech <strzelewicz.alexandre@gmail.com>
2015-05-18 17:29:42 -0400

    fix

--- README.md
+++ README.md
@@ -7,9 +7,10 @@ Sample module to automatically rotate logs of processes managed by PM2.
 
 ### Development mode
 
+Start an application with PM2 then
+
 ```
 $ git clone https://github.com/pm2-hive/pm2-logrotate.git
 $ cd pm2-logrotate
-$ npm install
-$ pm2 install .
+$ node app.js
 ```
--- app.js
+++ app.js
@@ -1,21 +1,28 @@
 
 var pmx     = require('pmx');
-var fs      = require('fs');
-var path    = require('path');
-var shelljs = require('shelljs');
-var pm2 = require('pm2');
+var pm2     = require('pm2');
 
 var conf = pmx.initModule();
 
+// Connect to local PM2
+pm2.connect(function() {
 
-function main() {
-  pm2.list(function() {
+  setInterval(function() {
 
-  });
-}
+    // Get process list managed by PM2
+    pm2.list(function(err, apps) {
 
-pm2.connect(main);
+      apps.forEach(function(app) {
 
-pmx.action('conf', function(reply) {
-  return reply(conf);
+        // Get error and out file
+        var out_file = app.pm2_env.pm_out_log_path;
+        var err_file = app.pm2_env.pm_err_log_path;
+
+        // Do some processing
+        console.log(out_file, err_file);
+      });
+
+    });
+  }, 1000);
+
 });
