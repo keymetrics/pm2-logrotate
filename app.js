06e505afdf4f1a23d196f47c2041a629d0a3133e
jshkurti <jshkurti@student.42.fr>
2015-06-02 15:22:48 +0200

    finished

--- app.js
+++ app.js
@@ -1,28 +1,68 @@
-
+var fs      = require('fs');
 var pmx     = require('pmx');
 var pm2     = require('pm2');
+var moment  = require('moment');
 
 var conf = pmx.initModule();
+var WORKER_INTERVAL = 1000 * 10; // 10seconds
+var SIZE_LIMIT = 1024 * 1024 * 5; // 5MB
+var TODAY = moment().format('DD');
+
+function proceed(file) {
+  var final_name = file.substr(0, file.length - 4) + '__'
+    + moment().format('MM-DD-YYYY-HH-mm-ss') + '.log';
+
+  var buffer = fs.readFileSync(file);
+  fs.writeFileSync(final_name, buffer);
+  buffer = null;
+
+  fs.truncateSync(file, 0);
+
+  console.log('"' + final_name + '" has been created');
+}
+
+function proceed_file(file, force) {
+  var size = fs.statSync(file).size;
+
+  if (size >= SIZE_LIMIT || force) {
+    proceed(file);
+  }
+}
+
+function proceed_app(app, force) {
+  // Get error and out file
+  var out_file = app.pm2_env.pm_out_log_path;
+  var err_file = app.pm2_env.pm_err_log_path;
+
+  proceed_file(out_file, force);
+  proceed_file(err_file, force);
+}
+
+function day_has_changed() {
+  if (TODAY === moment().format('DD')) {
+    return false;
+  }
+  else {
+    TODAY = moment().format('DD');
+    return true;
+  }
+}
 
 // Connect to local PM2
-pm2.connect(function() {
+pm2.connect(function(err) {
+  if (err) return console.error(err.stack || err);
 
-  setInterval(function() {
-
+  function worker() {
     // Get process list managed by PM2
     pm2.list(function(err, apps) {
+      if (err) return console.error(err.stack || err);
 
-      apps.forEach(function(app) {
-
-        // Get error and out file
-        var out_file = app.pm2_env.pm_out_log_path;
-        var err_file = app.pm2_env.pm_err_log_path;
-
-        // Do some processing
-        console.log(out_file, err_file);
-      });
-
+      if (day_has_changed())
+        apps.forEach(function(app) {proceed_app(app, true)});
+      else
+        apps.forEach(function(app) {proceed_app(app, false)});
     });
-  }, 1000);
-
+    setTimeout(worker, WORKER_INTERVAL);
+  };
+  worker();
 });
--- test.js
+++ test.js
@@ -1,58 +1,9 @@
+var MB = '';
 
+for (var i = 0; i < 1024 * 1024; ++i)
+  MB += '1';
 
-var stream = require('logrotate-stream')
-, toLogFile = stream({ file: './test.log', size: '100k', keep: 3 });
-
-var pm2 = require('pm2');
-var fs = require('fs');
-
-function copyFile(source, target, cb) {
-  var cbCalled = false;
-
-  var rd = fs.createReadStream(source);
-  rd.on("error", function(err) {
-    done(err);
-  });
-  var wr = fs.createWriteStream(target, {'flags': 'a'});
-  wr.on("error", function(err) {
-    done(err);
-  });
-  wr.on("close", function(ex) {
-    done();
-  });
-  rd.pipe(wr);
-
-  function done(err) {
-    if (!cbCalled) {
-      cb(err);
-      cbCalled = true;
-    }
-  }
-}
-function main() {
-
-  setInterval(function() {
-    pm2.list(function(err, apps) {
-      var out_file = apps[0].pm2_env.pm_out_log_path;
-      var size = fs.statSync(out_file).size;
-
-      console.log(size);
-      if (size > 1000) {
-        console.log(out_file);
-        copyFile(out_file, out_file + '-log', function() {
-          fs.truncate(out_file, 0, function(){
-            console.log('done');
-          });
-        });
-        // fs.createReadStream(out_file).pipe(fs.createWriteStream(out_file + '-log'));
-
-      }
-      //console.log(apps[0].pm2_env.pm_err_log_path);
-    });
-  }, 1000);
-}
-
-pm2.connect(main);
-
-
-//someStream.pipe(toLogFile);
+setInterval(function() {
+  process.stdout.write(MB);
+  process.stderr.write(MB);
+}, 1000);
